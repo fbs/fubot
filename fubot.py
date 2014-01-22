118a5f795e70c0dc9b7b8efe287e2f11b07ef1af
bas smit <bas@baslab.org>
2014-01-22 15:48:15 +0100

    Added support for config file

--- fubot.py
+++ fubot.py
@@ -5,8 +5,19 @@ from twisted.internet import protocol, reactor, ssl
 from twisted.python import log
 
 import sys
+import json
 
-class FuProtocol(irc.IRCClient):
+class FuProtocol(object, irc.IRCClient):
+    nickname = 'fubot'
+
+    lineRate = 0.3
+
+    # CTCP
+    versionName = 'fubot'
+    versionNum = '0.1'
+    versionEnv = 'loo nix'
+    sourceURL = '127.0.0.1'
+
     def __init__(self, reactor, bot, network):
         self.reactor = reactor
         self.bot = bot
@@ -31,24 +42,50 @@ class FuNetwork(protocol.ReconnectingClientFactory):
         proto = self.protocol(self.reactor, self.bot, self)
         return proto
 
-    def __init__(self, reactor, bot):
+    def __init__(self, reactor, bot, config):
         self.reactor = reactor
         self.bot = bot
+        self.config = config
 
     def connect(self):
-        self.reactor.connectTCP("localhost", 6667, self)
+        log.msg("Connection to networks")
+
+        if (self.config.get('ssl', 'False')) == 'True':
+            self.reactor.connectSSL(self.config['hostname'],
+                                    int(self.config.get('port', 6697)),
+                                    self,
+                                    ssl.ClientContextFactory())
+        else:
+            self.reactor.connectTCP(self.config['hostname'],
+                                    int(self.config.get('port', 6667)),
+                                    self)
+
+    ## Network name
+    @property
+    def name(self):
+        return self.config.get('name', 'fubot')
 
 class Fubot(object):
-    def __init__(self, reactor):
+    def __init__(self, reactor, conf_filename):
         self.reactor = reactor
+        self.connections = dict()
+        self.conf_filename = conf_filename
+        with open(conf_filename) as fd:
+            self.config = json.load(fd)
+        if self.config['logfile'] == 'stdin':
+            log.startLogging(sys.stdout)
+        else:
+            log.startLogging(open(self.config['logfile'],'a'))
 
     def start(self):
-        self.network = FuNetwork(self.reactor, self)
-        self.network.connect()
+        for nwconfig in self.config['networks']:
+            nw = FuNetwork(self.reactor, self, nwconfig)
+            self.connections[nw.name] = nw
+            nw.connect()
 
 def main():
-    log.startLogging(sys.stdout)
-    fubot = Fubot(reactor)
+    SETTINGS = 'settings.json'
+    fubot = Fubot(reactor, SETTINGS)
     reactor.callWhenRunning(fubot.start)
     reactor.run()
 
