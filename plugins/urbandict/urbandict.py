fe1d66dfa8ecd83d05b550e1982050ca2caea747
bas smit <bas@baslab.org>
2014-01-24 00:01:26 +0100

    Plugin urbandict improved

--- plugins/urbandict/urbandict.py
+++ plugins/urbandict/urbandict.py
@@ -1,6 +1,8 @@
 from zope.interface import implements
 from twisted.web.client import getPage
 from twisted.python import log
+from twisted.web import error as web_error
+from twisted.internet import error as internet_error
 
 import re
 import json
@@ -13,6 +15,23 @@ except ImportError:
 from core.interface import IPlugin, IMsgHandler
 from core.pluginmanager import plugin_manager
 
+def _make_apiurl(query):
+    APIURL = 'http://api.urbandictionary.com/v0/define?term=%s'
+    return parser.quote_plus(APIURL % query, ':=()/?')
+
+def _make_weburl(query):
+    WEBURL = 'http://www.urbandictionary.com/define.php?term=%s'
+    return parser.quote_plus(WEBURL % query, ':=()/?')
+
+# Stolen from somewhere
+def _repairjson(original):
+    original = original.replace(r'\r','').replace(r'\n','')
+    original = original.replace('\\x', '\\u00')
+    regex = re.compile(r'\\(?![/u"])')
+    fixed = regex.sub(r"\\\\", original)
+    return fixed
+
+
 class Urban(object):
     implements(IMsgHandler)
 
@@ -20,59 +39,55 @@ class Urban(object):
     version = '0.1'
     author = 'fbs'
     command = 'urban'
-    BASEURL = 'http://api.urbandictionary.com/v0/define?term=%s'
 
     def accepts_command(self, cmd):
         if cmd == self.command:
             return True
         return False
 
-    def _repairjson(self, original):
-        original = original.replace(r'\r','').replace(r'\n','')
-        original = original.replace('\\x', '\\u00')
-        regex = re.compile(r'\\(?![/u"])')
-        fixed = regex.sub(r"\\\\", original)
-        return fixed
-
     def handle(self, proto, user, channel, args):
         user = user[0]
-        url = self.BASEURL % (''.join(args))
-        url = parser.quote_plus(url, ':=()/?')
-        d = getPage(url)
+        self.query  = ' '.join(args)
+        self.weburl = _make_weburl(self.query)
+        self.apiurl = _make_apiurl(self.query)
 
-        d.addCallback(self.parse_json, proto, user, channel)
-        d.addErrback(self.fetch_failed, proto, user, channel)
-        d.addCallback(self.send_definition, proto, user, channel)
-        d.addErrback(self.send_definition_failed, proto, user, channel)
+        d = getPage(self.apiurl)
+        d.addCallback(self.handle_page, proto, user, channel)
+        d.addCallback(self.search_page, proto, user, channel)
+        d.addErrback(self.errback, proto, user, channel)
 
-    def parse_json(self, page, proto, user, channel):
-        jsondata = self._repairjson(page.decode('utf-8'))
+    def handle_page(self, page, proto, user, channel):
+        jsondata = _repairjson(page.decode('utf-8'))
         jsondata = json.loads(jsondata)
         return jsondata
 
-    def send_definition(self, page, proto, user, channel):
+    def search_page(self, page, proto, user, channel):
         results = page.get('result_type')
         definitions = page.get('list')
 
         if results == 'exact':
             output = definitions[0]['definition'].encode('ascii').strip()
+            log.msg(self.weburl)
+            proto.msg(channel, '%s: %s, see %s for more.' %
+                      (user, output, self.weburl))
         else:
-            output = "No results found"
-
-        proto.msg(channel, "%s: %s" % (user, output))
+            proto.msg(channel, '%s: Sorry, no results found' % user)
 
 
-    def parse_json_failed(self, failure, proto, user, channel):
-        proto.msg(channel, '%s: failed to parse JSON data' % user)
-        log.msg('Failed to parse json' % failure)
+    def errback(self, failure, proto, user, channel):
+        log.err('Plugin: %s: type: %s | message: %s' %
+                (self.name, failure.type, failure.getErrorMessage()))
 
-    def send_definition_failed(self, failure, proto, user, channel):
-        proto.msg(channel, '%s: failed to find definitions' % user)
-        log.msg('Failed to find definitions ', failure)
+        r = failure.check(web_error.Error,
+                         internet_error.ConnectError,
+                         ValueError)
+        if r == ValueError:
+            proto.msg(channel, '%s: Urban: Couldn\'t decode the JSON, sorry :(' % user)
+        elif r == web_error.Error:
+            proto.msg(channel,  '%s: Urban: Couldn\'t fetch the page, sorry :(' % user)
+        elif r == internet_error.ConnectError:
+            proto.msg(channel, '%s: Urban: Couldn\'t connect to the server, sorry :(' % user)
 
-    def fetch_failed(self, failure, proto, user, channel):
-        proto.msg(channel, '%s: Couldn\'t fetch json' % user)
-        log.msg("Failed to fetch urban json: ", failure)
 
 urban = Urban()
 plugin_manager.register(urban)
