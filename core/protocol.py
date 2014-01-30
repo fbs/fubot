75d043e63a7a8f017afc814479dbdd9a0e8eaefc
bas smit <bas@baslab.org>
2014-01-30 17:40:22 +0100

    Keep track of the channels joined/left

--- core/protocol.py
+++ core/protocol.py
@@ -13,6 +13,8 @@ class FuProtocol(object, irc.IRCClient):
     versionEnv = 'loonix'
     sourceURL = '127.0.0.1'
 
+    channels = None
+
     def __init__(self, reactor, bot, network):
         self.reactor = reactor
         self.bot = bot
@@ -31,15 +33,30 @@ class FuProtocol(object, irc.IRCClient):
 
     def connectionMade(self):
         self.network.connection = self
+        self.channels = []
         irc.IRCClient.connectionMade(self)
 
     def connectionLost(self, reason):
         log.msg('Lost connection to [%s]: %s' % (self.network.name, reason))
         self.network.connection = None
+        self.channels = []
         irc.IRCClient.connectionLost(self, reason)
 
     def privmsg(self, user, channel, message):
         self.bot.handle_privmsg(self, user, channel, message)
+
+    def joined(self, channel):
+        self.channels.append(channel)
+        log.msg('Joined channel ' + channel)
+
+    def left(self, channel):
+        self.channels.remove(channel)
+        log.msg('Left channel ' + channel)
+
+    def kickedFrom(self, channel, kicker, message):
+        self.channels.remove(channel)
+        log.msg('Kicked from channel %s by %s [%s]' %
+                (channel, kicker, message))
 
     def msg(self, user, message, length=None):
         if len(message) > 400:
